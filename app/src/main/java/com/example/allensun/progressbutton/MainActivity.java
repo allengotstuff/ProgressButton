8838673b19e5d07af44e3362f225b4702cd34294
Allen Sun <allengotstuff@gmail.com>
2017-09-06 18:15:07 -0700

    setup demo

--- app/src/main/java/com/example/allensun/progressbutton/MainActivity.java
+++ app/src/main/java/com/example/allensun/progressbutton/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.allensun.progressbutton;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.example.progessbuttonlib.ProgressView;
 
@@ -10,7 +11,29 @@ public class MainActivity extends AppCompatActivity {
 
     double count;
 
-    ProgressView progressView;
+    ProgressView progressView, progressView_1, progressView_2, progressView_3, progressView_4;
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            handler.post(task);
+        }
+    };
+
+    Runnable task = new Runnable() {
+        @Override
+        public void run() {
+            count += 1;
+            progressView.updateProgress(count);
+            progressView_1.updateProgress(count);
+            progressView_2.updateProgress(count);
+            progressView_3.updateProgress(count);
+            progressView_4.updateProgress(count);
+
+            handler.sendEmptyMessageDelayed(1, 40);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +41,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         progressView = (ProgressView) findViewById(R.id.ProgressView);
+        progressView_1 = (ProgressView) findViewById(R.id.ProgressView_1);
+        progressView_2 = (ProgressView) findViewById(R.id.ProgressView_2);
+        progressView_3 = (ProgressView) findViewById(R.id.ProgressView_3);
+        progressView_4 = (ProgressView) findViewById(R.id.ProgressView_4);
 
         count = 0;
 
-        progressView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                count += 2;
-                progressView.updateProgress(count);
-            }
-        });
+        handler.sendEmptyMessage(1);
     }
 }
